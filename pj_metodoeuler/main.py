1f7b312f8be27491db999fd14bd78116c8825760
ricardo lópez <rjlopezjobs@gmail.com>
2024-03-29 15:56:58 -0300

    testeo con la primera ecuacion del libro

--- pj_metodoeuler/main.py
+++ pj_metodoeuler/main.py
@@ -1,22 +1,36 @@
+import math
 
-def f(x,y):
-    return
 
 def mtd_euler(f,x0,y0,h,n):
-    "h es el paso"
 
     val_x = [x0]
     val_y = [y0]
+
 
     for i in range(n):
         x = val_x[-1]
         y = val_y[-1]
 
-        xn = n + h
-        yn = y + h*f(x,y)
+        xnew = x + h
+        ynew = y + h*f(x,y)
 
-        val_x.append(xn)
-        val_y.append(yn)
+        val_x.append(xnew)
+        val_y.append(ynew)
 
 
     return val_x, val_y
+
+"colocar funcion"
+def f(x,y):
+    return x*math.sqrt(y)
+
+"colocar condiciones iniciales"
+x0 = 1
+y0 = 4
+h = 0.1
+n = 6
+
+val_x,val_y = mtd_euler(f,x0,y0,h,n)
+
+for x,y in zip(val_x,val_y):
+    print(f'x = {x:.2f}, y= {y:.4f}')
