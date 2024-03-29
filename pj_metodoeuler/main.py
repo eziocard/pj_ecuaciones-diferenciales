7fe969f886a2ef18a831d09114256f87a9ada5ba
ricardo lópez <rjlopezjobs@gmail.com>
2024-03-29 14:36:27 -0300

    terminando de crear las funciones

--- pj_metodoeuler/main.py
+++ pj_metodoeuler/main.py
@@ -3,4 +3,17 @@ def f(x,y):
     return
 
 def mtd_euler(f,x0,y0,h,n):
-    return
+    "h es el paso"
+
+    val_x = [x0]
+    val_y = [y0]
+
+    for i in range(n):
+        x = val_x[-1]
+        y = val_y[-1]
+
+        xn = n + h
+        yn = y + h*f(x,y)
+
+
+    return val_x, val_y
