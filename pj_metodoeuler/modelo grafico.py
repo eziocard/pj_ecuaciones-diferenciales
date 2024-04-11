0e9c6814e48a4bcbcf082a637403fbebb1a4afc7
ricardo lópez <rjlopezjobs@gmail.com>
2024-04-10 21:49:58 -0400

    agregando texto de temperatura

--- pj_metodoeuler/modelo grafico.py
+++ pj_metodoeuler/modelo grafico.py
@@ -7,24 +7,26 @@ pygame.init()
 ventana = pygame.display.set_mode((800,600))
 pygame.display.set_caption("ley de enfriamiento")
 
-"color del fondo"
+"definicion de parametros"
 blanco = (255,255,255)
 rojo = (255,0,0)
 negro = (0,0,0)
 amarillo = 	(255, 255, 0)
-posicion_texto = (100, 100)
+posicion_texto = (100,100)
 
-"fuente del texto"
-font = pygame.font.Font(None, 36)
-text_temperatura = font.render("temperatura:", True,negro)
-ventana.blit(text_temperatura,True,posicion_texto)
-"creacion de la tasa"
-ventana.fill(blanco)
-pygame.draw.rect(ventana,rojo,(350,499,100,100))
 bar_x = 50
 bar_y = 50
 bar_width = 700
 bar_height = 20
+
+"fuente del texto"
+font = pygame.font.Font(None, 36)
+texto = font.render("Temperatura",True,negro)
+
+"mostrar en pantalla"
+ventana.fill(blanco)
+ventana.blit(texto,(12,15))
+pygame.draw.rect(ventana,rojo,(350,499,100,100))
 pygame.draw.rect(ventana, amarillo, (bar_x, bar_y, 60,bar_height))
 
 # Dibuja el borde de la barra de carga
